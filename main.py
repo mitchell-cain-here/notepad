1bb48ea0bb6f40956b910b143a4c4257e07ca491
MitchellRevamp <mitchellcainhere2024@gmail.com>
2025-07-25 22:56:15 -0400

    Update main.py
    Added Open Button

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox, filedialog
+from tkinter import filedialog as fd
 
 def save_to_file():
     file_path = filedialog.asksaveasfilename(defaultextension=".txt", filetypes=[("Text files", "*.txt"), ("All files", "*.*")])
@@ -24,4 +25,16 @@ save_button.pack(pady=10)
 
 status_label = tk.Label(root, text="", padx=10, pady=10, bg="#000000")
 status_label.pack()
+
+def open_text_file():
+    filetypes = (
+        ('text files', '*.txt'),
+        ('All files', '*.*')
+    )
+    f = fd.askopenfile(filetypes=filetypes)
+    text_widget.insert('1.0', f.readlines())
+
+# To Do - Make this clear text after opening a file and adjust button height 
+open_button = tk.Button(root,text='Open a File',command=open_text_file)
+open_button.pack(pady=10)
 root.mainloop()
